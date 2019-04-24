0c5377a5b6f3718f0414e68af464805d411ceda3
vishwasshenoy <vishwas.shenoy@gmail.com>
2019-04-23 23:27:38 -0500

    Updates

---
+++ precache-manifest.370c2a1b0b21f4b655c61e0ba00e961c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f55482c5a47f3d05047835752f5690c",
+    "url": "/Gurukul/static/media/mysore2.0f55482c.jpg"
+  },
+  {
+    "revision": "4cf2d95dc75de5204d89b7fa71155906",
+    "url": "/Gurukul/static/media/mysore1.4cf2d95d.jpg"
+  },
+  {
+    "revision": "3b1bb7e518585c0f973d",
+    "url": "/Gurukul/static/js/runtime~main.ab82e911.js"
+  },
+  {
+    "revision": "81a1dd64dc6dfe9476b6",
+    "url": "/Gurukul/static/js/main.3ff4d913.chunk.js"
+  },
+  {
+    "revision": "66dffa048399a2584157",
+    "url": "/Gurukul/static/js/2.3ec86ba4.chunk.js"
+  },
+  {
+    "revision": "66dffa048399a2584157",
+    "url": "/Gurukul/static/css/2.d40f410d.chunk.css"
+  },
+  {
+    "revision": "85652a6ab9f6f240e26c65f6e1f25ec1",
+    "url": "/Gurukul/index.html"
+  }
+];
